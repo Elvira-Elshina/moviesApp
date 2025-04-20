f4d9e0ec9fe88c97db58e2922ab3bf67e25379b4
Elvira Elshina <elvircikfrolenochka@gmail.com>
2025-04-20 18:50:14 +0700

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,103 @@
 import { React, useState, useEffect } from "react";
 import { Offline, Online } from "react-detect-offline";
-import { Button, Alert } from "antd";
-import ButtonGenre from "./components/buttonGenre/buttonGenre";
+import { Pagination, Spin, Input, Tabs, Alert } from "antd";
 import FilmCard from "./components/filmCard/FilmCard";
+import debounce from "lodash/debounce";
 
 function MoviesApp() {
+  const [searchValue, setSearchValue] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(1);
+
+  const url = "";
+  const APIKey = "";
+  const language = "";
+  const query = "";
+  useEffect(() => {
+    setLoading(true);
+
+    const response = fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${searchValue}&page=${page}`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTdkZmJmODcxYWI2YjVjYmYxMGNiZGU3NjAzNzM3YSIsIm5iZiI6MTczOTI4MzkyOS45ODIsInN1YiI6IjY3YWI1ZGQ5NTE4NDA4YWJmOGJiMmY2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FgFAODEHWdNUPjWEQa3cemqDHPIm5luKFRzYcrXSwq0",
+        },
+      }
+    );
+
+    response
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.total_pages);
+        setTotalPages(res.total_pages);
+        setSearchResult(res.results);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setErr(true);
+      });
+  }, [searchValue, page]);
+
+  const onPaginationChange = (e) => setPage(e);
+
   return (
     <div id="generalContainer">
-      <Online>
-        <ul>
-          <FilmCard />
-        </ul>
-      </Online>
-      <Offline>
-        <Alert
-          message="Отсутствует подключение к интернету"
-          type="warning"
-          showIcon
+      {/* <Online> */}
+      <form id="searchContainer">
+        <Tabs
+          defaultActiveKey="Search"
+          items={[
+            {
+              label: "Search",
+              key: "1",
+            },
+            {
+              label: "Rated",
+              key: "2",
+            },
+          ]}
         />
-      </Offline>
+        <Input
+          type="search"
+          placeholder="Type to search..."
+          onChange={debounce((e) => {
+            const val = e.target.value.replace(/ /g, "+");
+            setSearchValue(val);
+          }, 500)}
+        />
+      </form>
+      <ul>
+        {err ? <Alert message="Error!" type="error" showIcon /> : null}
+        {loading ? (
+          <Spin />
+        ) : (
+          <FilmCard
+            searchValue={searchValue}
+            searchResult={searchResult}
+            loading={loading}
+          />
+        )}
+      </ul>
+      <Pagination
+        showSizeChanger={false}
+        total={totalPages}
+        onChange={onPaginationChange}
+        pageSize={10}
+      />
+      {/* </Online> */}
+      {/* <Offline> */}
+      {/* <Alert
+        message="Отсутствует подключение к интернету"
+        type="warning"
+        showIcon
+      /> */}
+      {/* </Offline> */}
     </div>
   );
 }
--- src/components/filmCard/FilmCard.jsx
+++ src/components/filmCard/FilmCard.jsx
@@ -5,42 +5,8 @@ import "./FilmCard.css";
 import { parseISO, format } from "date-fns";
 import { enGB } from "date-fns/locale";
 
-function FilmCard() {
-  let data = "";
-  const apiKey = "api_key=217dfbf871ab6b5cbf10cbde7603737a";
-
+function FilmCard({ searchValue, searchResult }) {
   const [someFilms, setSomeFilms] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [err, setErr] = useState(false);
-
-  // const [filmTitle, setFilmTitle] = useState("");
-  // const [filmDescription, setFilmDescription] = useState("");
-  // const [filmDate, setFilmDate] = useState("");
-
-  useEffect(() => {
-    const result = fetch(
-      // `https://api.themoviedb.org/3/movie/${data}?${apiKey}`,
-      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
-      {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTdkZmJmODcxYWI2YjVjYmYxMGNiZGU3NjAzNzM3YSIsIm5iZiI6MTczOTI4MzkyOS45ODIsInN1YiI6IjY3YWI1ZGQ5NTE4NDA4YWJmOGJiMmY2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FgFAODEHWdNUPjWEQa3cemqDHPIm5luKFRzYcrXSwq0",
-        },
-      }
-    );
-    result
-      .then((res) => res.json())
-      .then((resp) => {
-        const arrFilms = resp.results.slice(0);
-        setSomeFilms(arrFilms);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setErr(true);
-      });
-  }, []);
 
   function formatText(text) {
     if (text.length >= 200) {
@@ -57,9 +23,15 @@ function FilmCard() {
     return text;
   }
 
+  useEffect(() => {
+    const arr = searchResult.slice(0);
+    setSomeFilms(searchResult);
+  }, [searchResult]);
+
   const filmCards = someFilms.map((el, i) => {
     const imageMovie = `https://image.tmdb.org/t/p/original/${el.poster_path}`;
-    const dateF = el.release_date;
+    let dateF = el.release_date;
+
     const liFilm = (
       <li key={i}>
         <img id="image" src={imageMovie} />
@@ -68,7 +40,9 @@ function FilmCard() {
           <h3 id="movieTitle">{el.title}</h3>
           <div id="movieDate">
             {" "}
-            {format(parseISO(dateF), "LLLL d, yyyy", { locale: enGB })}
+            {dateF
+              ? format(parseISO(dateF), "LLLL d, yyyy", { locale: enGB })
+              : null}
           </div>
           <ButtonGenre />
           <ButtonGenre />
@@ -79,11 +53,16 @@ function FilmCard() {
 
     return liFilm;
   });
+  // console.log(searchValue);
 
-  if (err) {
-    return <Alert message="Error" type="error" showIcon />;
-  } else if (loading) {
-    return <Spin />;
+  if (searchValue && someFilms.length === 0) {
+    return (
+      <Alert
+        message="Ничего не найдёно по данному запросу"
+        type="info"
+        showIcon
+      />
+    );
   } else {
     return filmCards;
   }
